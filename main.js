d220dff14774a200f42358ba6c7eb095a153d2ff
P-Saval <pablo.saval88@gmail.com>
2025-04-05 18:40:37 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -3,11 +3,17 @@ import "./src/styles/main.scss"
 
 import { Header } from "./src/components/Header/Header";
 import { Gallery } from "./src/components/Gallery/Gallery";
+import { displayPhotos } from "./src/components/Card/Card";
  
 
 const app = document.querySelector("#app");
 
 const header = Header();
+
 const gallery = Gallery();
+
 app.append(header, gallery);    
+
+displayPhotos("Welcome");
+
 
